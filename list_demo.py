dca98fc2552a97ee3eec2c6c73a0b002a52cc77e
Michael <michaelpaul3154@gmail.com>
2025-05-15 14:45:43 +0100

    Add list_demo.py for learning about list and tuple and manipulation also

---
+++ list_demo.py
@@ -0,0 +1,18 @@
+#Working with a list
+fruits = ["lemon", "carrot", "cucumber", "berry"]
+print(f"List of fruit available: {fruits}")
+
+fruits.append("orange")
+print(f"Adding more fruit: {fruits}")
+
+fruits.remove("lemon")
+print(f"Remaining fruits after buying last lemon: {fruits}")
+
+print(f"First fruit: {fruits[0]}")
+print(f"Number of fruits available: {len(fruits)}")
+
+#Working with tuple
+foods = ("Rice", "Yam", "Beans", "Cassava Flake")
+print(f"List of foods available: {foods}")
+
+print(f"Second food: {foods[1]}")
