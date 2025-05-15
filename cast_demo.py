c8313f2be791a5832f3fa75a7000252abc6d6501
Michael <michaelpaul3154@gmail.com>
2025-05-15 14:00:48 +0100

    Add cast_demo.py demostrating type casting

---
+++ cast_demo.py
@@ -0,0 +1,16 @@
+
+# cast_demo.py
+
+a = "10"
+b = 3.5
+c = int(a)
+d = int(b)
+e = str(d)
+f = float(a)
+
+print("Original:", a, b)
+print("Int from string:", c)
+print("Int from float:", d)
+print("String from int:", e)
+print("Float from string:", f)
+
