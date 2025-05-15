f06d765223d46d2f84dd278543041fa2120586b0
Michael <michaelpaul3154@gmail.com>
2025-05-15 14:09:18 +0100

    Add even_odd.py to check if a number is even or odd

---
+++ even_odd.py
@@ -0,0 +1,10 @@
+def check_even_odd(num):
+	if num % 2 == 0:
+		print(f"{num} is an even number")
+	else:
+		print(f"{num} is an odd number")
+
+check_even_odd(12)
+check_even_odd(15)
+check_even_odd(58)
+
